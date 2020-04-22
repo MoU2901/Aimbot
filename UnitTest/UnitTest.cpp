600410df618e9573a09e089155261ac356a3b245
MoU2901 <mo290103@gmail.com>
2020-04-22 10:53:25 +0200

    Added unit test

---
+++ UnitTest/UnitTest.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../AimBot/Point.h"
+#include "../AimBot/Point.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(UnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Assert::AreEqual(Point{ 0, 100, 0 }.distance(Point{ 0, 2, 0 }), 98.0f);
+		}
+	};
+}
